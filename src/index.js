638e06d9db2ce3b98fee594ae76f7e4560e239b0
nginnever <ginneversource@gmail.com>
2018-05-09 09:44:52 -0700

    construct web3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 'use strict'
 
+const Web3 = require('web3')
+const web3 = new Web3()
+
 exports = module.exports
 
 class Layer2lib {
   constructor(provider) {
-
+    web3.setProvider(new web3.providers.HttpProvider(provider))
+    console.log(web3.eth.accounts[0])
   }
 }
 
