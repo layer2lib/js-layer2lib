1c60f64a6bf89d0ea2cb16490d8676e22a7db41b
nginnever <ginneversource@gmail.com>
2018-05-09 11:15:26 -0700

    modular

--- src/general-state-channel.js
+++ src/general-state-channel.js
@@ -1,1 +1,12 @@
 'use strict'
+
+module.exports = function gsc (self) {
+  return {
+    get: async function() {
+
+    },
+    set: async function() {
+      
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@
 
 const Web3 = require('web3')
 const web3 = new Web3()
+const GSC = require('./general-state-channel')
+
+// const config = require('./config')
+// replaced by repo-browser when running in browser
+// const defaultRepo = require('./runtime/repo-nodejs')
 
 exports = module.exports
 
 class Layer2lib {
   constructor(provider) {
+    this.gsc = GSC(this)
+    
     web3.setProvider(new web3.providers.HttpProvider(provider))
   }
 
