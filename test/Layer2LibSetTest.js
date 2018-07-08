3a64588e5a54d6a435005ecae101c57f0726a9d9
nginnever <ginneversource@gmail.com>
2018-07-08 12:36:20 -0700

    instance still cross updating

--- test/Layer2LibSetTest.js
+++ test/Layer2LibSetTest.js
@@ -13,14 +13,16 @@ const Layer2lib = require('../src/index.js')
 const GunProxy = require('layer2storage').GunStorageProxy
 
 const gun = new Gun({ radisk: false, localStorage: true })
+const gun1 = new Gun({ radisk: false, localStorage: true })
+const gun2 = new Gun({ radisk: false, localStorage: true })
 
-async function test(_db) {
+async function test(_db1, _db2, _db3) {
   let _partyA = '0xd4EA3b21C312D7C6a1c744927a6F80Fe226A8416'
   let _partyB = 'b1dd709d7eb8138f25b71103d41d50ba8708e816'
   let _partyI = '0x1e8524370b7caf8dc62e3effbca04ccc8e493ffe'
 
   // ALICE
-  const proxyAlice = new GunProxy(_db, `layer2/Alice`);
+  const proxyAlice = new GunProxy(_db1, `layer2/Alice`);
   let optionsAlice = {
     db: proxyAlice,
     privateKey: '0x9eb0e84b7cadfcbbec8d49ae7112b25e0c1cb158ecd2160c301afa1f4a1029c8'
@@ -43,7 +45,7 @@ async function test(_db) {
 
 
   // Bob
-  const proxyBob = new GunProxy(_db, `layer2/Bob`);
+  const proxyBob = new GunProxy(_db2, `layer2/Bob`);
   let optionsBob = {
     db: proxyBob,
     privateKey: '0x42b83487fcc52252abd33f7c1d32006545388d0036e1ed3ae75c86a62a5c85d1'
@@ -65,7 +67,7 @@ async function test(_db) {
 
 
   // Ingrid
-  const proxyIngrid = new GunProxy(_db, `layer2/Ingrid`);
+  const proxyIngrid = new GunProxy(_db3, `layer2/Ingrid`);
   let optionsIngrid = {
     db: proxyIngrid,
     privateKey: '0x2c339e1afdbfd0b724a4793bf73ec3a4c235cceb131dcd60824a06cefbef9875'
@@ -127,4 +129,4 @@ async function test(_db) {
   await lIngrid.setPayment.confirmCloseLC(id)
 }
 
-test(gun)
+test(gun, gun1, gun2)
